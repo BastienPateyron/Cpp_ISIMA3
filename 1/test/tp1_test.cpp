ecc279a5c69f724e0697cbe3f0028a1a556be29d
bapateyron <bastien.pateyron@etu.isima.fr>
2020-10-12 14:05:52 +0200

    Nuage OK

--- 1/test/tp1_test.cpp
+++ 1/test/tp1_test.cpp
@@ -243,7 +243,7 @@ TEST_CASE ( "TP1_Point::ConversionVersCartesien_V2" ) {
 }
 
 //----------------------------------------------------------------------------------------------- 18
-/*TEST_CASE ( "TP1_Nuage::Ajout" ) {
+TEST_CASE ( "TP1_Nuage::Ajout" ) {
  Cartesien p1(12.0,24.0);
  Polaire p2(13.0,25.0);
  Polaire p3(p1);
@@ -259,7 +259,7 @@ TEST_CASE ( "TP1_Point::ConversionVersCartesien_V2" ) {
  n.ajouter(p4);
 
  REQUIRE ( n.size() == 4u );
-}*/
+}
 
 //----------------------------------------------------------------------------------------------- 19
 /*TEST_CASE ( "TP1_Nuage::Iterateurs" ) {
