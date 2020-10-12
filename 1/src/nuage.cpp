9e5b3df3b2189c100396a893c13637fba7865c94
bapateyron <bastien.pateyron@etu.isima.fr>
2020-10-12 14:42:55 +0200

    Nuage::Iterator

--- 1/src/nuage.cpp
+++ 1/src/nuage.cpp
@@ -2,3 +2,5 @@
 
 unsigned int Nuage::size() { return (unsigned int) points.size();}
 void Nuage::ajouter(Point & p) { points.push_back(&p); }
+Nuage::const_iterator Nuage::begin() { return points.begin();}
+Nuage::const_iterator Nuage::end() { return points.end();}
--- 1/src/nuage.hpp
+++ 1/src/nuage.hpp
@@ -6,16 +6,25 @@
 #include "cartesien.hpp"
 #include "polaire.hpp"
 #include <vector>
+#include <iterator>
+
+
 
 class Nuage {
+
 
    private:
       std::vector<Point *> points;
 
    public:
+      using const_iterator = std::vector<Point *>::const_iterator;
+
       unsigned int size();
       void ajouter(Point & p);
+      const_iterator begin();
+      const_iterator end();
 
+      
 };
 
 #endif
