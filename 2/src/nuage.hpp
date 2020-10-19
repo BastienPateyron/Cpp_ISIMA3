1c58e6e257601ef21047c83963d2ae27d55d11dc
bapateyron <bastien.pateyron@etu.isima.fr>
2020-10-19 15:27:57 +0200

    TP2: Barycentre en cours

--- 2/src/nuage.hpp
+++ 2/src/nuage.hpp
@@ -8,29 +8,48 @@
 #include <vector>
 #include <iterator>
 
-template<class Cartesien> class Nuage;
-template<class Polaire> class Nuage;
+// template<class Cartesien> class Nuage;
+// template<class Polaire> class Nuage;
 
 
 template <typename T> class Nuage {
 
    private:
-      // std::vector<Point *> points;
       std::vector<T> points;
    
    public:
-      // using const_iterator = std::vector<Point *>::const_iterator;
       using const_iterator = typename std::vector<T>::const_iterator;
 
       unsigned int size() { return (unsigned int) points.size(); }
       void ajouter(const T & p) { points.push_back(p); }
       const_iterator begin() { return points.begin(); }
       const_iterator end() { return points.end(); }
-      // T barycentre(Nuage<T> & n);
 };
 
+template <typename T> T barycentre_v1(Nuage<T> & n) {
+   double x = 0.0, y = 0.0;
 
+   if(n.size() == 0) {
+      return T(x, y);
+   }
+   else {
+      typename Nuage<T>::const_iterator it;
 
+      for(it = n.begin(); it != n.end(); ++it) {
+         Cartesien tmp;
+
+         (*it).convertir(tmp);
+         x += tmp.getX();
+         y += tmp.getY();
+      }
+
+      Cartesien c(x/n.size(), y/n.size());
+      T resultat;
+
+      return c;
+   }
+
+}
 
 
 // class BarycentreCartesien {
